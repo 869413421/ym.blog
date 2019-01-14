db802508ae4d6de6468e2e7bdc4f62e2a576836a
869413421 <13528685024@163.com>
2019-01-14 22:32:49 +0800

    更新配置

--- ym.blog/config/blog.php
+++ ym.blog/config/blog.php
@@ -1,14 +1,14 @@
 <?php
 return [
-    'title' => 'Ym Blog',
-    'posts_per_page' => 5,
-    'subtitle' => 'www.qingshuinisha.cn',
-    'description' => '清水洗泥沙',
+    'name' => "ym.blog",
+    'title' => "清水洗泥沙",
+    'subtitle' => '',
+    'description' => '',
     'author' => 'ym',
     'page_image' => 'home-bg.jpg',
     'posts_per_page' => 10,
     'uploads' => [
         'storage' => 'public',
-        'webpath' => '/storage/',
+        'webpath' => '/storage/uploads',
     ],
 ];
