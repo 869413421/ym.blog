8eef9b94744138bbafb48ce2fbfcccf1d35449b9
869413421 <13528685024@163.com>
2019-01-14 22:56:24 +0800

    aa

--- ym.blog/config/blog.php
+++ ym.blog/config/blog.php
@@ -2,13 +2,13 @@
 return [
     'name' => "ym.blog",
     'title' => "清水洗泥沙",
-    'subtitle' => '',
+    'subtitle' => 'ym.blog',
     'description' => '',
     'author' => 'ym',
     'page_image' => 'home-bg.jpg',
     'posts_per_page' => 10,
     'uploads' => [
         'storage' => 'public',
-        'webpath' => '/storage/',
+        'webpath' => '/storage/uploads',
     ],
 ];
