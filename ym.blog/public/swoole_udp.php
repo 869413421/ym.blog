0362b6236324a7d540c0991f0863a94c02e6dc96
869413421 <13528685024@163.com>
2019-01-16 21:29:10 +0800

    httptest

--- ym.blog/public/swoole_udp.php
+++ ym.blog/public/swoole_udp.php
@@ -6,8 +6,8 @@ $port = 8988;
 $server = new \swoole_server($host, $port, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
 $server->on('packet', function ($server, $data, $fd) {
-    $server->sendto($fd['address'], $fd['port'], 'Server:' . $data);
     var_dump($fd['address']);
+    $server->sendto($fd['address'], $fd['port'], 'Server:' . $data);
 });
 
 $server->start();
