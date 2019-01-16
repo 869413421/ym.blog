c2d77ec0768d2de9dc25f666a970ec58d3abe273
869413421 <13528685024@163.com>
2019-01-16 20:39:37 +0800

    修改UDP

--- ym.blog/public/swoole_udp.php
+++ ym.blog/public/swoole_udp.php
@@ -5,7 +5,9 @@ $port = '8988';
 
 $server = new \swoole_server($host, $port, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 
-$server->on('packect', function ($server, $data, $fd) {
+$server->on('packet', function ($server, $data, $fd) {
     $server->sendto($fd['address'], $fd['port'], 'Server:' . $data);
     var_dump($fd['address']);
 });
+
+$server->start();
